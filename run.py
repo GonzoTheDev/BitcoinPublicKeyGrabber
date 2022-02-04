4b98d296f338e33aa3d529e122a13861bd4c4d22
Gonzo <72076492+GonzoTheDev@users.noreply.github.com>
2022-02-04 22:03:22 +0000

    Create run.py

---
+++ run.py
@@ -0,0 +1,21 @@
+import requests
+import csv
+mylist = []
+
+with open('btc_base58.txt', newline='', encoding='utf-8') as f:
+    for line in f:
+        mylist.append(line.strip())
+
+myfile = open('pubkeys.txt', 'w')
+
+for i in range(0,len(mylist)):
+    address = mylist[i]
+    link = f"https://blockchain.info/q/pubkeyaddr/{address}"
+    f = requests.get(link)
+    if(f.text == ''):
+        pass
+    else:
+        myfile.write("%s\n" % f.text)
+        print(f.text)
+
+myfile.close()
